7d2a6ce029da22bf49cfa1614a7bd88988c74b2b
ggail01 <tyzmaineleandre@gmail.com>
2023-10-19 10:05:09 +0800

    completed learning03

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Fraction
+{
+    private int _top = 0;
+    private int _bottom = 0;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string _fraction = $"{_top}/{_bottom}";
+        return _fraction;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine();
+
+        Fraction f2 = new Fraction(8);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine();
     }
 }
